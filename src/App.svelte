d28d4cfc335bae2bda8b201f14c49f8ee89b6c10
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2023-01-21 20:36:39 -0300

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
   import Router from "../node_modules/svelte-spa-router";
   import About from "./About.svelte";
   import Help from "./Help.svelte";
+  import AddForm from "./AddForm.svelte";
+  import Tabela from "./Tabela.svelte";
   export let name;
 
   console.log(db);
@@ -14,14 +16,28 @@
   <nav>
     <a href="/#/about">About</a>
     <a href="/#/help">Help</a>
+    <a href="/#/tabela">Tabela</a>
+    <a href="/#/addForm">Adicionar</a>
   </nav>
   <Router
     routes={{
       "/about": About,
       "/help": Help,
+      "/tabela": Tabela,
+      "/addForm": AddForm,
     }}
   />
+  <br />
+  <div id="form">
+    <slot />
+    <!--Here enter the components -->
+  </div>
 </main>
 
 <style>
+  #form {
+    display: flex;
+    justify-content: left;
+    margin-left: 10%;
+  }
 </style>
---
+++ src/Form.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { TextInput } from "carbon-components-svelte";
+</script>
+
+<main>
+  <TextInput labelText="Nome" placeholder="Digite nome do paciente ... " />
+</main>
